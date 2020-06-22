66fb70360db67cd14b3e283d6b59d14c134cf6e8
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-06-22 18:03:10 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+void decode() {
+	int a[100], n;
+	cin >> n;
+	int mn = INT_MAX;
+	for(int i = 0; i < n; ++i) {
+		cin >> a[i];
+		mn = min(mn, a[i]);
+	}
+	bool ok = true;
+	sort(a, a + n);
+	for(int i = n - 1; i >= 0; --i) {
+		if(i == 0) break;
+		else if(a[i] - a[i-1] <= 1) {
+			ok = false;
+		}
+	}
+	if(!ok) cout << 2;
+	else cout << 1;
+	cout << "\n";
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		decode();
+	}
+	return 0;
+}
